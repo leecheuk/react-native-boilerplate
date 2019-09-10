e0236ce23107c4a33fc1751c3c77c0358f12278b
m <dev.cheuk.lee@gmail.com>
2019-09-10 01:08:51 -0400

    Added navigator and store

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import notificationReducer from './redux/NotificationReducer';
+import TabNavigator from './components/TabNavigator';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const store = createStore(notificationReducer)
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <TabNavigator />
+      </Provider>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
